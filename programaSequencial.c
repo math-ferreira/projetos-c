51eec9c0c933ed20ccdee78ff2feb6b79c77aa16
reidomath <mat.s.ferreira@gmail.com>
2019-04-06 19:13:26 -0300

    Programa Sequencial Primos

--- programaSequencial.c
+++ programaSequencial.c
@@ -1,5 +1,5 @@
 /*Programação Paralela, prof Emilio
-Projeto 1 - Programa Sequencial
+Projeto 1 - Programa Sequencial - Projeto Time M&M
 https://github.com/ufabc-bcc/2019_Q1_PP_MPI-projetotimem-m
 */
 
@@ -8,37 +8,35 @@ https://github.com/ufabc-bcc/2019_Q1_PP_MPI-projetotimem-m
 #include <time.h>
 
 int main() {
+    clock_t start = clock(); //Inicia a contagem do tempo para o Programa
 
-  // Apresenta mensagem inicial para usuario entrar com valor N
-  // para limite superior até qual todos os números primos deverão ser gerados
-  printf("Digite seu limite superior dos numeros primos:\n");
+    // Apresenta mensagem inicial para usuario entrar com valor N
+    // para limite superior até qual todos os números primos deverão ser gerados
+    printf("Digite seu limite superior dos numeros primos: ");
 
-  //O tipo de dado short requer 16 bits para ser implementado (10^6 até 10^11).
-  long n;
-  scanf("%ld", &n);
+    //O tipo de dado short requer 16 bits para ser implementado (10^6 até 10^11).
+    long n, aux=1, soma=0;
+    scanf("%ld", &n);
 
-  srand(time(NULL));
-  int data[n];
-
-  for (int i = 0; i < n; i++){
-      data[i] = i+2;    
-  }
-
-  for (int j = 0; j < n; j++){
-      printf("data[%d] = %d"), j, data[j];    
-  }
-
-
-
-  // clock_t start = clock();
-  // long long sum = 0;
-  // for (int i = 0; i < 100000; i++){
-  //   for (int c = 0; c < arraySize; ++c) {
-  //     if (data[c] >= 128)
-  //       sum += data[c];
-  //   }
-  // }
-  // double elapsedTime = (double)(clock() - start) / CLOCKS_PER_SEC;
-  // printf ("Tempo total: %lf\n", elapsedTime);
-  // printf ("Soma: %lld\n", sum);
+    srand(time(NULL));
+    long arrayP[n]; // Declara 'arrayPrimo', que sera o Array dos numeros PRIMOS de p a n, tal que [2p, 3p, 4p... n]
+    for (long i=0; i < n; i++){
+        arrayP[i] = i+2;
+    }
+    while (aux < n){ //Percorre um laço até o valor limite n definido pelo usuario
+        aux++; //Variavel auxiliar para servir de parametro para verificar o numero primo ou não (resto da divisão)
+        for(long i=0; i < n; i++){
+            if (arrayP[i]%aux == 0 && arrayP[i] > aux && arrayP[i] != -1){ // Caso condição seja True, signifca que não é primo
+                arrayP[i] = -1;
+            } 
+        }
+    }
+    for(long i=0; i < n; i++){
+        if (arrayP[i] != -1 && arrayP[i] <= n){
+            soma++;
+        }
+    }
+    printf("%ld\n", soma);
+    double tempoTotal = (double)(clock() - start) / CLOCKS_PER_SEC;
+    printf ("Tempo total do programa: %lf\n", tempoTotal);
 }
